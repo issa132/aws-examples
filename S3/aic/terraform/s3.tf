469c082a91806c6a99dc50b9894576e185fb29a5
issa132 <issa132@hotmail.com>
2026-03-08 11:30:14 +0000

    terraform simple s3 example

--- S3/aic/terraform/s3.tf
+++
@@ -1,7 +0,0 @@
-# https://github.com/github/gitignore/blob/main/Terraform.gitignore
-resource "aws_s3_bucket" "my-s3-bucket" {
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
