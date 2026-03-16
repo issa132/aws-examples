8e79e3da49acb25f70a207e2ef82a13175042bb9
issa132 <issa132@hotmail.com>
2026-03-16 18:12:43 +0000

    ruby example

--- S3/sdk/ruby/s3.rb
+++ S3/sdk/ruby/s3.rb
@@ -3,6 +3,8 @@ require 'aws-sdk-s3'    # AWS SDK for S3 operations
 require 'pry'           # Interactive shell for debugging
 require 'securerandom'  # For generating secure random strings (UUIDs)
 
+# to execute the program you do bundle exec ruby s3.rb
+
 # S3 Bucket configuration
 bucket_name = ENV['BUCKET_NAME']  # Fetching bucket name from environment variables
 region = 'ca-central-1'           # AWS region where the bucket will be created
